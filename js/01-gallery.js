0a481dc3c8da5ccf43af497c9c1db0bf99d65fcf
Milena Bolaños <mpbolanos0@gmail.com>
2023-10-28 08:34:41 -0500

    Se realizan los script de la tarea 1 y 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,122 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const photosMarkup = createGalleryItem(galleryItems);
+
+function createGalleryItem(element){
+    return element
+    .map(({preview, original, description}) => {
+        return `
+            <li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img 
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                </a>
+            </li>`
+    })
+    .join('');
+};
+
+const galleryHandler = (event) => {
+    event.preventDefault();
+
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    };
+
+    const originalUrl = event.target.dataset.source;
+    const instance = basicLightbox.create(`<img src="${originalUrl}">`)
+    instance.show();
+
+    window.addEventListener('keydown', onEscPress);
+    
+    function onEscPress(event){
+        const ESC_KEY_CODE = 'Escape';
+        if (event.code === ESC_KEY_CODE) {
+            instance.close();
+        };
+    };
+};
+
+galleryContainer.insertAdjacentHTML("beforeend", photosMarkup);
+galleryContainer.addEventListener('click', galleryHandler);
+
+
+/*
+
+Tarea 1 - Galería de imágenes
+
+Cree una galería con la posibilidad de hacer clic en los elementos y ver una imagen a tamaño completo 
+en una ventana modal. Vea un vídeo demo de la galería.
+
+Ejecute esta tarea en los archivos 01-gallery.html y 01-gallery.js. Desglóselo en varias subtareas:
+
+Creación y renderización de la marca basada en el array de datos galleryItems y la plantilla de elementos 
+de la galería proporcionada.
+Implementación de la delegación a ul.gallery y obtención de url de un gran imagen.
+Conectar el script de la ventana modal y los estilos de la biblioteca basicLightbox. 
+Utilice CDN servicio jsdelivr y añadir referencias a archivos de biblioteca minificados (.min) al proyecto.
+Abra una ventana modal haciendo clic en un elemento de la galería. Para ello, 
+consulte documentación y los ejemplos.
+Sustitución del atributo src del elemento <img> en la ventana modal antes de apertura. 
+Use el diseño ya hecho de la ventana modal con la imagen de los ejemplos de la biblioteca basicLightbox.
+Marcar el elemento de la galería
+El enlace a la imagen original debe almacenarse en data-atributo source del elemento <img>, 
+y debe estar especificado en el href del enlace. No añada ningún otro tag HTML o distintas clases CSS en esta plantilla.
+
+<li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</li>
+
+Tenga en cuenta que la imagen está envuelta en un enlace, lo que significa que cuando el usuario 
+hace clic será redirigido a otra página por defecto. Desactive este comportamiento por defecto.
+
+Cierre desde el teclado
+ATENCIÓN
+La siguiente funcionalidad no es obligatoria a la hora de entregar una tarea, pero sería 
+una buena práctica adicional.
+
+Añada cerrar la ventana modal pulsando Escape. Hágao de manera que el teclado sólo se escuche 
+mientras la ventana modal este abierta. En la biblioteca basicLightbox existe un método 
+para para cerrar la ventana modal de forma programada.
+
+EXPLICACIÓN
+
+Este código crea una galería de imágenes que muestra una imagen ampliada al hacer clic en ella y permite cerrarla al presionar la tecla "Escape". De la siguiente manera:
+
+1. Se importa el objeto `galleryItems` de un archivo llamado `'./gallery-items.js'`.
+
+2. Se selecciona el contenedor de la galería utilizando `document.querySelector()`.
+
+3. Se llama a la función `createGalleryItem(galleryItems)` y asigna el resultado a la constante `photosMarkup`.
+
+4. Se define la función `createGalleryItem(element)` que toma un elemento y crea el markup HTML para cada item de la galería utilizando el método `map()` en el array `element`.
+
+5. Se crea un evento `galleryHandler` que se activa al hacer clic en un elemento de la galería.
+
+6. Dentro de `galleryHandler`, se verifica si el elemento clicado es una imagen (`event.target.nodeName === 'IMG'`).
+
+7. Si es una imagen, obtiene la URL original de la imagen a través del atributo `data-source` del elemento y crea una instancia de `basicLightbox` con esa URL.
+
+8. Muestra la instancia de `basicLightbox`.
+
+9. Se agrega un evento de escucha para capturar cuando se presiona la tecla "Escape" (`keydown`) y cierra la instancia de `basicLightbox` si se detecta esa tecla.
+
+10. Se inserta el markup HTML generado en el paso 3 dentro del contenedor de la galería utilizando `insertAdjacentHTML()`.
+
+11. Se agrega un evento de clic en el contenedor de la galería que activa la función `galleryHandler`.
+
+
+*/
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,82 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const photosMarkup = createGalleryItem(galleryItems);
+
+function createGalleryItem(element){
+    return element
+    .map(({preview, original, description}) => {
+        return `
+            <a class="gallery__item" href="${original}">
+                <img 
+                    class="gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                />
+            </a>`
+    })
+    .join('');
+};
+
+galleryContainer.insertAdjacentHTML("beforeend", photosMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a');
+
+
+/*
+
+Tarea 2 - biblioteca SimpleLightbox
+
+Cree la misma galería que en la primera tarea, pero usando la biblioteca SimpleLightbox, 
+que se encargará de manejar los clics en las imágenes, abriendo y cerrando la ventana modal, 
+y desplazándose por las imágenes con el teclado.
+
+Si necesita cambiar ligeramente el diseño de la tarjeta de la galería, use esta plantilla.
+
+<li class="gallery__item">
+   <a class="gallery__link" href="large-image.jpg">
+      <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+   </a>
+</li>
+
+Ejecute esta tarea en los archivos 02-lightbox.html y 02-lightbox.js. Divídalo en varias subtareas:
+
+Creación y visualización de marcas a partir del conjunto de datos galleryItems y la plantilla de elementos 
+de la galería proporcionada. Use el código terminado de la primera asignación.
+Añada al script y los estilos de la biblioteca usando CDN servicio cdnjs. Debe añadir los enlaces a los 
+dos archivos: simple-lightbox.min.js y simple-lightbox.min.css.
+Inicialización de la biblioteca después de crear y añadir los elementos de la galería en un ul.gallery. 
+Para ello, lea la documentación SimpleLightbox , principalmente de las secciones «Usage» y «Markup».
+Mire la sección «Opciones» de la documentación y añada una pantalla de subtítulos las imágenes del 
+atributo alt. Deje que el pie de foto esté en la parte inferior y aparezca a 250 milisegundos 
+después de abrir la imagen.
+
+EXPLICACIÓN
+
+Este código crea una galería de imágenes utilizando los datos del array `galleryItems`. 
+Cada imagen se muestra como un enlace en la galería, y al hacer clic en una imagen 
+se abre en un lightbox para verla en tamaño completo.. De la siguiente manera:
+
+1. Se importa el array `galleryItems` desde un archivo externo.
+
+2. Se selecciona el contenedor de la galería en el DOM utilizando `document.querySelector(".gallery")`.
+
+3. Se define una función llamada `createGalleryItem` que toma como argumento el array `galleryItems`.
+
+4. Se utiliza el método `map` en el array `galleryItems` para iterar sobre cada elemento y
+ generar un fragmento de código HTML para cada imagen de la galería.
+
+5. Cada fragmento de código HTML consiste en un enlace `<a>` con una imagen `<img>` dentro, 
+donde se establecen los atributos `href`, `src` y `alt` utilizando los datos del elemento correspondiente en el array.
+
+6. Se utiliza el método `join('')` para convertir los fragmentos de código HTML en una sola cadena de texto.
+
+7. Se inserta la cadena de texto generada en el contenedor de la galería 
+utilizando `galleryContainer.insertAdjacentHTML("beforeend", photosMarkup)`.
+
+8. Se crea una instancia de la clase `SimpleLightbox`, pasando como argumento 
+el selector CSS que apunta a los elementos `<a>` dentro de la galería (`'.gallery a'`). 
+Esto inicializa el lightbox para que funcione con las imágenes de la galería.
+
+*/
